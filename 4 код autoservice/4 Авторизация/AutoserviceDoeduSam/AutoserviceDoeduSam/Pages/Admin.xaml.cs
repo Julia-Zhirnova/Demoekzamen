e5c78581b2bfd342a6d94520596b807c062e2924
Julia-Zhirnova <you@example.com>
2022-10-25 20:46:41 +0300

    role

--- 4 код autoservice/4 Авторизация/AutoserviceDoeduSam/AutoserviceDoeduSam/Pages/Admin.xaml.cs
+++ 4 код autoservice/4 Авторизация/AutoserviceDoeduSam/AutoserviceDoeduSam/Pages/Admin.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoserviceDoeduSam.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,54 @@ namespace AutoserviceDoeduSam.Pages
         public Admin()
         {
             InitializeComponent();
+            AppConnect.modelOdb=new Models.AvtorisationEntities();
+        }
+
+        private void BtnInLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = AppConnect.modelOdb.User.FirstOrDefault(x =>
+                x.Login == TxbLogin.Text && x.Password == PsbPassword.Password);
+                if(userObj == null)
+                {
+                    MessageBox.Show("Такого пользователя нет!", "Ошибка при авторизации",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("Здравствуйте " + userObj.Role.Name + ", " + userObj.Name, "Уведомление",
+                       MessageBoxButton.OK, MessageBoxImage.Information);
+                    switch (userObj.IdRole)
+                        {
+                            case 1:
+                                Class.Manager.MainFrame.Navigate(new Administrator());
+                                break;
+                            case 2:
+                                Class.Manager.MainFrame.Navigate(new StarshiiSmen());
+                                break;
+                            case 3:
+                                Class.Manager.MainFrame.Navigate(new Prodavec());
+                                break;
+                        default:
+                                MessageBox.Show("Данные не обнаружены!", "Уведомление",
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+                                break;
+
+
+                        }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message.ToString(), "Критическая работа приложения",
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void BtnRegIn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
